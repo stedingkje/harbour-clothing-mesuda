840c4186dd5da405bda6bb5896aa160ba6ebb287
Leroy Steding <leroysteding@gmail.com>
2023-02-04 14:25:54 +0100

    debugg

--- src/modules/checkout/components/payment-container/index.tsx
+++ src/modules/checkout/components/payment-container/index.tsx
@@ -37,6 +37,7 @@ const PaymentContainer: React.FC<PaymentContainerProps> = ({
   setSelected,
   disabled = false,
 }) => {
+  console.log("ps", paymentSession)
   return (
     <div
       className={clsx(
@@ -54,10 +55,11 @@ const PaymentContainer: React.FC<PaymentContainerProps> = ({
         <Radio checked={selected} />
         <div className="flex flex-col text-left">
           <h3 className="text-base-semi leading-none text-gray-900">
+            {console.log(paymentSession)}
             {/* {PaymentInfoMap[paymentSession.provider_id].title} */}
           </h3>
           <span className="text-gray-700 text-small-regular mt-2">
-            {PaymentInfoMap[paymentSession.provider_id].description}
+            {/* {PaymentInfoMap[paymentSession.provider_id].description} */}
           </span>
           {selected && (
             <div className="w-full mt-4">
