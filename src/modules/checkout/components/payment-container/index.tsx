84e33f29c6848b241fdb943ac2e32a553b312ab7
Leroy Steding <leroysteding@gmail.com>
2023-02-04 14:28:54 +0100

    debug

--- src/modules/checkout/components/payment-container/index.tsx
+++ src/modules/checkout/components/payment-container/index.tsx
@@ -55,7 +55,6 @@ const PaymentContainer: React.FC<PaymentContainerProps> = ({
         <Radio checked={selected} />
         <div className="flex flex-col text-left">
           <h3 className="text-base-semi leading-none text-gray-900">
-            {console.log(paymentSession)}
             {/* {PaymentInfoMap[paymentSession.provider_id].title} */}
           </h3>
           <span className="text-gray-700 text-small-regular mt-2">
