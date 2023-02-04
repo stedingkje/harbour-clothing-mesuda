694f8a552d9aaebe1739796f17167e8b55d1a1de
Leroy Steding <leroysteding@gmail.com>
2023-02-04 14:36:15 +0100

    Stripe fix

--- src/modules/checkout/components/payment-container/index.tsx
+++ src/modules/checkout/components/payment-container/index.tsx
@@ -21,6 +21,22 @@ const PaymentInfoMap: Record<string, { title: string; description: string }> = {
     title: "iDEAL",
     description: "Secure payment with iDEAL",
   },
+  "stripe-bancontact": {
+    title: "Bancontact",
+    description: "Secure payment with Bancontact",
+  },
+  "stripe-blik": {
+    title: "Blik",
+    description: "Secure payment with Blik",
+  },
+  "stripe-giropay": {
+    title: "Giropay",
+    description: "Secure payment with Giropay",
+  },
+  "stripe-przelewy24": {
+    title: "przelewy24",
+    description: "Secure payment with przelewy24",
+  },
   paypal: {
     title: "PayPal",
     description: "Secure payment with PayPal",
@@ -55,10 +71,10 @@ const PaymentContainer: React.FC<PaymentContainerProps> = ({
         <Radio checked={selected} />
         <div className="flex flex-col text-left">
           <h3 className="text-base-semi leading-none text-gray-900">
-            {/* {PaymentInfoMap[paymentSession.provider_id].title} */}
+            {PaymentInfoMap[paymentSession.provider_id].title}
           </h3>
           <span className="text-gray-700 text-small-regular mt-2">
-            {/* {PaymentInfoMap[paymentSession.provider_id].description} */}
+            {PaymentInfoMap[paymentSession.provider_id].description}
           </span>
           {selected && (
             <div className="w-full mt-4">
